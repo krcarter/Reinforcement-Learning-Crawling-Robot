e6f0d153cbd95fedf2ccdb0fc91a14c8f533eccd
Kamal Carter <kamal.carter@yahoo.com>
2024-07-05 23:15:49 -0400

    Rename created URDF with better joint and link naming scheme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Crawling-Robot
+
  Reinforcement learning for crawling robot
+
+ # Hardware
+
+ URDF, CAD files, and robot BOM can be found in the hardware folder. Currently the pybullet code reference the urdf from the pybullet directory, so you can either change that reference in the code or move the urdf and mesh files to the pybullet directory you are working on.
+
+ To create your own custom URDF for bullet using fusion 360 for CAD checkout this github repo
+
+ * https://github.com/yanshil/Fusion2PyBullet
+
+# Dependencies
+
+* Pybullet
+* Microsoft C++ Build Tools
--- main/loaf_urdf.py
+++ main/loaf_urdf.py
@@ -6,21 +6,16 @@ import os
 current_path = os.path.dirname(os.path.realpath(__file__))
 print("Current path:", current_path)
 
-
 def load_and_visualize_urdf(urdf_path):
     # Connect to PyBullet
-    p.connect(p.GUI)
+    physicsClient = p.connect(p.GUI)
 
     p.configureDebugVisualizer(p.COV_ENABLE_SHADOWS, 1)
     p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)
 
-
-
     # Set the path to PyBullet's data directory
     print("Pybullet Path: ", pybullet_data.getDataPath())
-    #path = str(Users\My PC\Desktop\Projects\CrawlingRobot\urdf_test)
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
-    #p.setAdditionalSearchPath(path)
 
     # Load the URDF file
     urdf_id = p.loadURDF(urdf_path)
@@ -32,19 +27,22 @@ def load_and_visualize_urdf(urdf_path):
     plane_id = p.loadURDF("plane.urdf")
 
     # Set the initial position and orientation of the URDF
-    initial_position = [0, 0, 1]
-    initial_orientation = p.getQuaternionFromEuler([0, 0, 0])
+    initial_position = [0, 0, 1] #x,y,z
+    initial_orientation = p.getQuaternionFromEuler([0, 0, 0]) # rpy?
     p.resetBasePositionAndOrientation(urdf_id, initial_position, initial_orientation)
 
     # Run the simulation
     while True:
         p.stepSimulation()
         time.sleep(1./240.)
+        crawlyPos, crawlyOrn = p.getBasePositionAndOrientation(urdf_id)
+        #print(crawlyPos,crawlyOrn)
+
+
 
     # Disconnect from PyBullet
     p.disconnect()
 
 # Path to your URDF file
-#urdf_path = "urdf_test.urdf"  # Change this to your URDF file path
 urdf_path = "crawly/crawly.urdf"  # Change this to your URDF file path
 load_and_visualize_urdf(urdf_path)
