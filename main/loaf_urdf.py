29fcaba1ed8444b1fbda31ce550a8a747f2b6ac0
Kamal Carter <kamal.carter@yahoo.com>
2024-07-06 02:22:31 -0400

    Modified URDF for joint are numbered properly adding some debugging code for load urdf

--- main/loaf_urdf.py
+++ main/loaf_urdf.py
@@ -17,31 +17,48 @@ def load_and_visualize_urdf(urdf_path):
     print("Pybullet Path: ", pybullet_data.getDataPath())
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-    # Load the URDF file
-    urdf_id = p.loadURDF(urdf_path)
-
     # Set the gravity
     p.setGravity(0, 0, -9.81)
 
     # Create a plane for the URDF to stand on
     plane_id = p.loadURDF("plane.urdf")
 
+    # Load the URDF file
+    urdf_id = p.loadURDF(urdf_path)
+    num_joint = p.getNumJoints(urdf_id)
+
+    ## INFO DEBUGGING ##
+    # joint_order = [1,5,2,6,3,7,4,8]
+    # Why is there a joint upper limit for the continous joints???????
+
+    print("NumJoints: ", p.getNumJoints(urdf_id))
+    initial_joint_angles = [0, 0, 0, 0, 0, 0, 0, 0]
+
+    for joint in range(p.getNumJoints(urdf_id)):
+        #print('JointInfo' + str(joint) + ": ", p.getJointInfo(urdf_id, joint))
+         p.resetJointState(urdf_id, joint, initial_joint_angles[joint])
+
+    ## INFO DEBUGGING End ##
+
+    #print("JointInfo: ", p.getJointInfo(urdf_id))
     # Set the initial position and orientation of the URDF
     initial_position = [0, 0, 1] #x,y,z
-    initial_orientation = p.getQuaternionFromEuler([0, 0, 0]) # rpy?
+    initial_orientation = p.getQuaternionFromEuler([0, 0, 0]) # XYZW - rpy?
     p.resetBasePositionAndOrientation(urdf_id, initial_position, initial_orientation)
 
     # Run the simulation
     while True:
         p.stepSimulation()
         time.sleep(1./240.)
+
+        # LOGGGING #
         crawlyPos, crawlyOrn = p.getBasePositionAndOrientation(urdf_id)
         #print(crawlyPos,crawlyOrn)
 
 
 
     # Disconnect from PyBullet
-    p.disconnect()
+    p.disconnect() # won't reach here because while loop never breaks at the moment
 
 # Path to your URDF file
 urdf_path = "crawly/crawly.urdf"  # Change this to your URDF file path
