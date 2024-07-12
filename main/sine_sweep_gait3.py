ed7c401c4a45ffc5ff89ace744ce4a2fa743b26e
krcarter <kamal.carter@yahoo.com>
2024-07-12 18:50:03 -0400

    code cleanup

--- main/sine_sweep_gait3.py
+++ main/sine_sweep_gait3.py
@@ -5,15 +5,12 @@ import numpy as np
 import time
 import os
 
+L1 = 0.1 # m
+L2 = 0.1 # m
 
 def fk(th1, th2):
-    l1 = 0.1 # m
-    l2 = 0.1 # m
-
-    #th1 = th1 - np.pi/2
-
-    x = l1 * np.cos(th1) + (l1 + l2) * np.cos(th1 + th2)
-    y = l2 * np.sin(th1) + (l1 + l2) * np.sin(th1 + th2)
+    x = L1 * np.cos(th1) + (L1 + L2) * np.cos(th1 + th2)
+    y = L2 * np.sin(th1) + (L1 + L2) * np.sin(th1 + th2)
 
     plt.figure(figsize=(10, 6))
     plt.plot(x, y, label='End-Effector Trajectory')
@@ -27,23 +24,55 @@ def fk(th1, th2):
     return (x,y)
 
 def ik(x, y):
-    l1 = 0.1 # m
-    l2 = 0.1 # m
 
-    th2 = np.arccos((x**2 + y**2 - l1**2 - l2**2)/(2*l1*l2))
-    th1 = np.arctan(y/x) - np.tan( (l2*np.sin(th2))/ (l1 + l2*np.cos(th2)))
+    # Calculate the distance from the origin to the point (x, y)
+    r = np.sqrt(x**2 + y**2)
 
-    return (th1, th2)
+    # Check if the point is reachable
+    if r > (L1 + L2) or r < np.abs(L1 - L2):
+        raise ValueError("The point (x, y) is not reachable with the given link lengths.")
 
+    # Calculate the angle theta2 using the law of cosines
+    cos_theta2 = (x**2 + y**2 - L1**2 - L2**2) / (2 * L1 * L2)
+    sin_theta2 = np.sqrt(1 - cos_theta2**2)  # sin(theta2) could be positive or negative
+
+    theta2 = np.arctan2(sin_theta2, cos_theta2)
+
+    # Calculate the angle theta1
+    k1 = L1 + L2 * cos_theta2
+    k2 = L2 * sin_theta2
+
+    theta1 = np.arctan2(y, x) - np.arctan2(k2, k1)
+    
+    return (theta1, theta2)
+
+def robot_debug(urdf_id,joint_index_list):
+    print('Joint States: ')
+    joint_state = p.getJointStates(urdf_id, joint_index_list)
+    print(joint_state)
+
+    print('Link States: ')
+    link_states = p.getLinkStates(urdf_id, joint_index_list)
+    print(link_states)
+    
+    print('Base Velocity: ')
+    base_velocity = p.getBaseVelocity(urdf_id)
+    print(base_velocity)
+
+    print('Num Bodies: ')
+    print(p.getNumBodies())
+
+    print('Body Info: ')
+    print(p.getBodyInfo)
 
 def plot_trajectory(time, trajectories):
     
     #Plot the trajectories
     plt.figure(figsize=(12, 8))
     num_joint = len(trajectories)
-    print('num_joint: ', num_joint)
+    #print('num_joint: ', num_joint)
     for joint in range(num_joint):
-        print(joint)
+        #print(joint)
         plt.plot(time, trajectories[joint], label=f'Joint {joint+1}')
 
     plt.xlabel('Time [s]')
@@ -53,14 +82,12 @@ def plot_trajectory(time, trajectories):
     plt.grid(True)
     plt.show()
 
-
-
 def crawl_walk(time):
-    #
     intiial_position = [-np.pi/12, np.pi/2, np.pi/12, -np.pi/2, np.pi/2, np.pi/12, -np.pi/2, -np.pi/12]
+
     # Define the parameters for the circular sweep
     sweep_duration = time  # duration of the sweep in seconds
-    frequency = 10.0       # frequency of the sine wave (1 cycle per sweep_duration)
+    frequency = 10.0       # frequency of the sine wave (ideally velocity control)
 
     # Define the simulation timestep
     timestep = 1.0 / 240.0
@@ -72,14 +99,12 @@ def crawl_walk(time):
     # Generate initial empy list of trajectories
     # Repeat the values to fill an 8xsteps array
     trajectories = np.tile(intiial_position, (num_steps, 1)).T
-    
-
 
     # Generate the trajectory using a sine wave
     omega = 2 * np.pi * frequency
 
-    Amp = [np.pi/12, np.pi/6, np.pi/12, np.pi/6, 0, 0, 0, 0] 
-    phi = [np.pi/4, np.pi/4, np.pi/4, np.pi/4, np.pi/2, np.pi/2, np.pi/2, np.pi/2]
+    Amp  = [np.pi/12, np.pi/6, np.pi/12, np.pi/6, 0, 0, 0, 0] 
+    phi  = [np.pi/4, np.pi/4, np.pi/4, np.pi/4, np.pi/2, np.pi/2, np.pi/2, np.pi/2]
     Amp0 = [-np.pi/12, (5/12)*np.pi, np.pi/12, -(5/12)*np.pi/2, 0, 0, 0, 0]
     thetas = np.tile(intiial_position, (num_steps, 1)).T
 
@@ -129,60 +154,17 @@ def load_and_visualize_urdf(urdf_path):
     # Create a plane for the URDF to stand on
     plane_id = p.loadURDF("plane.urdf")
 
-    #Set Friction Properties
+    #Set Friction Properties of Ground Plan
     p.changeDynamics(plane_id, -1, lateralFriction=1.0)
-    print('Dynamics info: ', p.getDynamicsInfo(plane_id, -1))
 
     # Load the URDF file
     flags = p.URDF_USE_SELF_COLLISION | p.URDF_USE_SELF_COLLISION_INCLUDE_PARENT
     urdf_id = p.loadURDF(urdf_path, flags=flags)
-    #urdf_id = p.loadURDF(urdf_path)
     num_joint = p.getNumJoints(urdf_id)
     joint_index_list = list(range(num_joint))
-    #Set Friction Properties
-    print('Dynamics info crawly: ', p.getDynamicsInfo(urdf_id, 0))
-    print(joint_index_list)
 
-    ## INFO DEBUGGING ##
-    '''
-    for joint in range(num_joint):
-        p.enableJointForceTorqueSensor(urdf_id, joint, 1)
-        print('JointInfo' + str(joint) + ": ", p.getJointInfo(urdf_id, joint))
-    print('Joint States')
-    joint_state = p.getJointStates(urdf_id, joint_index_list)
-    # position
-     print(joint_state)
-    '''
+    # robot_debug(urdf_id, joint_index_list) # print statements
 
-    # print('Joint States: ')
-    # joint_state = p.getJointStates(urdf_id, joint_index_list)
-    # print(joint_state)
-
-    # print('Link States: ')
-    # link_states = p.getLinkStates(urdf_id, joint_index_list)
-    # print(link_states)
-    
-    # print('Base Velocity: ')
-    # base_velocity = p.getBaseVelocity(urdf_id)
-    # print(base_velocity)
-
-    # print('Num Bodies: ')
-    # print(p.getNumBodies())
-
-    # print('Body Info: ')
-    # print(p.getBodyInfo)
-
-
-    # Commanding Joints
-    # p.setJointMotorControlArray(
-    #     bodyUniqueId = urdf_id, 
-    #     jointIndices = joint_index_list,
-    #     controlMode = p.POSITION_CONTROL,
-    #     targetPositions = initial_joint_angles)
-
-    ## INFO DEBUGGING End ##
-
-    #print("JointInfo: ", p.getJointInfo(urdf_id))
     # Set the initial position and orientation of the URDF
     initial_position = [0, 0, 1] #x,y,z
     initial_orientation = p.getQuaternionFromEuler([np.pi/2, 0, 0]) # XYZW - rpy?
@@ -190,13 +172,11 @@ def load_and_visualize_urdf(urdf_path):
 
     print("NumJoints: ", p.getNumJoints(urdf_id))
     initial_joint_angles = [np.pi/2, 0, -np.pi/2, 0, np.pi/2, 0, -np.pi/2, 0] # laying flat
-    #initial_joint_angles = [np.pi/4, -np.pi/2, -np.pi/4, np.pi/2, -np.pi/4, np.pi/2, np.pi/4, -np.pi/2] # standing up
 
     for joint in range(num_joint):
         p.resetJointState(urdf_id, joint,initial_joint_angles[joint])
         p.enableJointForceTorqueSensor(urdf_id, joint, 1)
         print('JointInfo' + str(joint) + ": ", p.getJointInfo(urdf_id, joint))
-
 
     #Generate Trajectory
     sweep_duration = 5.0 #seconds
@@ -206,7 +186,6 @@ def load_and_visualize_urdf(urdf_path):
     timestep = 1.0 / 240.0
     num_steps = int(sweep_duration / timestep)
 
-    joint_index = 0
 
     # Run the simulation
     while True:
@@ -228,20 +207,11 @@ def load_and_visualize_urdf(urdf_path):
                 controlMode = p.POSITION_CONTROL,
                 targetPositions = target_position)
 
-            # # Apply the target joint position to the robot
-            # p.setJointMotorControl2(
-            #     bodyUniqueId=urdf_id,
-            #     jointIndex=joint_index,
-            #     controlMode=p.POSITION_CONTROL,
-            #     targetPosition=target_position
-            # )
-      
-            # Step the simulation
-            p.stepSimulation()
             #crawlyPos, crawlyOrn = p.getBasePositionAndOrientation(urdf_id)
             #print(crawlyPos,crawlyOrn)
-
-
+            #       
+            # Step the simulation
+            p.stepSimulation()
 
     # Disconnect from PyBullet
     #p.disconnect() # won't reach here because while loop never breaks at the moment
