3f14b6cab3224407d8f7130f3e9841dfc722638e
Kamal Carter <kamal.carter@yahoo.com>
2024-07-19 19:19:05 -0400

    Need to use gym==0.23.1 with the latest pybullet version (3.2.6)

--- main/test_pybullet_envs.py
+++ main/test_pybullet_envs.py
@@ -4,15 +4,20 @@ import pybullet_envs
 def main():
     # Create the gym environment
     env = gym.make('MinitaurBulletEnv-v0')
-    env.render(mode='human')
 
     # Reset the environment to start
     observation = env.reset()
+
+    env.render(mode='human')
+
 
     # Run the simulation for 1000 steps
     for _ in range(1000):
         # Sample a random action from the action space
         action = env.action_space.sample()
+
+        print(type(action))
+        print(action)
 
         # Take a step in the environment
         observation, reward, done, info = env.step(action)
