0e29d18022b7252345889ae3b1a898e1775bdec1
Kamal Carter <kamal.carter@yahoo.com>
2024-07-24 21:11:23 -0400

    Pass arguments in the eval function

--- main/robot_evaluate.py
+++ main/robot_evaluate.py
@@ -1,18 +1,29 @@
-# evaluate.py
+import argparse
 from stable_baselines3 import PPO
 from pybullet_envs.robot_env import RobotEnv
 import time
 
-# Load the trained model
-model = PPO.load("models/ppo_robot")
+def evaluate_model(model_path, render):
+    # Load the trained model
+    model = PPO.load(model_path)
 
-# Create the environment
-env = RobotEnv("urdf/crawly.urdf", render = True)
+    # Create the environment
+    env = RobotEnv("urdf/crawly.urdf", render = render)
 
-# Test the trained model
-obs = env.reset()
-for _ in range(10000):
-    #time.sleep(2)
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    env.render()
+    # Test the trained model
+    obs = env.reset()
+    for _ in range(10000):
+        #time.sleep(2)
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = env.step(action)
+        env.render()
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description="Evaluate a trained PPO model")
+    parser.add_argument("--render", type=bool, default=True, help="Render the environment")
+    parser.add_argument("--model_path", type=str, required=True, help="Path to the trained model")
+
+    args = parser.parse_args()
+
+    evaluate_model(args.model_path, args.render)
